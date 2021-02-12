4e0a03c8441fd72f99a9c5e58d1ead9cd1a6c4c4
NetologyQA12 <dvb90@bk.ru>
2021-02-12 16:34:36 +0700

    fix likes

--- src/main/java/ru/netology/domain/Likes.java
+++ src/main/java/ru/netology/domain/Likes.java
@@ -1,31 +1,31 @@
 package ru.netology.domain;
 
 public class Likes {
-    private int count; //— число пользователей, которым понравилась запись;
-    private int userLikes; //[0,1] — наличие отметки «Мне нравится» от текущего пользователя (1 — есть, 0 — нет);
+    private int likesCount; //— число пользователей, которым понравилась запись;
+    private boolean userLikes; //[0,1] — наличие отметки «Мне нравится» от текущего пользователя (1 — есть, 0 — нет);
     private boolean canLike;  //[0,1] — информация о том, может ли текущий пользователь поставить отметку «Мне нравится» (1 — может, 0 — не может);
     private boolean canPublish;  //[0,1] — информация о том, может ли текущий пользователь сделать репост записи (1 — может, 0 — не может).
 
-    public Likes(int count, int userLikes, boolean canLike, boolean canPublish) {
-        this.count = count;
+    public Likes(int likesCount, boolean userLikes, boolean canLike, boolean canPublish) {
+        this.likesCount = likesCount;
         this.userLikes = userLikes;
         this.canLike = canLike;
         this.canPublish = canPublish;
     }
 
     public int getCount() {
-        return count;
+        return likesCount;
     }
 
     public void setCount(int count) {
-        this.count = count;
+        this.likesCount = count;
     }
 
-    public int getUserLikes() {
+    public boolean getUserLikes() {
         return userLikes;
     }
 
-    public void setUserLikes(int userLikes) {
+    public void setUserLikes(boolean userLikes) {
         this.userLikes = userLikes;
     }
 
